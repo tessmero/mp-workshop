a15c4eb4ab930b9e4221596c034dcee128d2f58a
Oliver Tessmer <oliver.tessmer@gmail.com>
2025-10-13 22:03:01 -0400

    initial commit

---
+++ src/frameworks-data.ts
@@ -0,0 +1,26 @@
+/**
+ * @file frameworks data
+ */
+export const ALL_FRAMEWORKS = {
+  engines: {
+    scratch: {
+      label: 'Homebrew',
+    },
+    react: {
+      label: 'react',
+      homepage: 'https://react.dev/',
+    },
+    svelte: {
+      label: 'svelte',
+      homepage: 'https://svelte.dev/',
+    },
+    angular: {
+      label: 'angular',
+      homepage: 'https://angular.dev/',
+    },
+    vue: {
+        label: 'vue',
+        homepage: 'https://vuejs.org/',
+    }
+  },
+};
